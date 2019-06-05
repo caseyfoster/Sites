1cd040fad08e971c6c92c18fc9c7188d748a0cee
caseyfoster <me@caseyfoster.net>
2019-06-05 19:03:47 -0400

    first push
    probs not work

---
+++ apps.js
@@ -0,0 +1,727 @@
+// var myFirst = "Casey"; //my name
+// var myDog = "Chloe";   //my dogs name
+
+// // var imSmart = false;
+// // console.log(imSmart);
+
+// var imSmart = true;
+
+// if (imSmart) {
+//     alert("you are smart")
+//     console.log("imSmart = true");
+// }
+// else {
+//     alert("you are not smart")
+//     console.log("imSmart = false")
+// }
+
+
+// // alert("The best dogs name in the world is: " + myDog);
+
+// console.log(myDog);
+
+// myDog = "Chloe 2";
+
+// console.log(myDog);
+
+
+// const myMessage = "welcome";
+
+// if(true){
+//     let myMessage = "new one";
+//     myMessage = "changed it from new one again"
+//     console.log(myMessage);
+// }
+
+// myMessage = "changed welcome to not welcome"
+// console.log(myMessage);
+
+// const message = true;
+// console.log(message);
+// if (message) {
+//     message = "new message"
+//  }
+//  console.log(message);
+
+// var message;
+// console.log(message);
+// message = "this is the new message";
+// console.log(message);
+
+// var phoneVar = false;
+// console.log(phoneVar);
+// phoneVar = false;
+// if (phoneVar) {
+//     console.log("phonevar is true");}
+// else{
+//     console.log("phonevar is not true")
+// }
+
+// var message = true;
+// console.log(message);
+// if (message){
+//     let message2 = "new message";
+    
+// }
+// else;
+//     console.log("this is false")
+// console.log(message);
+
+// var firstName = "Casey";
+// console.log(firstName);
+
+// if (firstName){
+//     firstName = "casey 2nd first name"
+
+// }
+// console.log(firstName);
+// firstName = "Casey new 2nd name"
+// console.log(firstName);
+
+// const test = false;
+// let message = "this is our welcome message";
+// let moreStuff;
+// if (test){
+//     console.log(message);
+//     moreStuff = "this is some more contnet";
+//     let inBlock = "this is a secret message in the block"
+//     console.log(inBlock);
+// }
+// else {
+//     console.log(message + " this is all of the code that executes because false");
+// }
+// console.log(moreStuff);
+
+// let userName = window.prompt("What is your username");
+// let message = "welcome to the site "
+
+// console.log(message + userName);
+// let firstName = prompt("what is your first name?");
+// let lastName = prompt("what is your last name?");
+// let message = `Welcome ${firstName.charAt(0).toUpperCase() + firstName.slice(1)} ${lastName.charAt(0).toUpperCase() + lastName.slice(1)}`;
+
+// console.log(message);
+
+// let temp = 6;
+// const dong = 3;
+
+// dong = 1;
+// // temp = 10;
+// // temp = String(10);
+// // temp = String([1,2,3,4]);
+// rofl = temp - dong;
+
+// console.log(rofl);
+
+// let howMuchMoney = prompt("how much money do you have?");
+// let howMuchSpend = prompt("how much money did you spend?");
+
+// let leftOver = howMuchMoney - howMuchSpend;
+
+// let math = `You have $${leftOver} left over`;
+
+// console.log(math);
+
+// let a = "10";
+// a = Number(a);
+// a = String("10");
+// console.log(a);
+// console.log(typeof a);
+
+
+// let a = Number("5");
+// let b = Number("10");
+
+// console.log(a+b);
+
+
+// let firstNum = 5;
+// let secondNum = 10;
+// firstNum ++;
+// secondNum --;
+// let total = firstNum + secondNum;
+// console.log(total);
+// total = 500 + 100 / 5 + total;
+// console.log(total);
+
+// let miles = prompt("How many miles?");
+// miles = Number(miles);
+// console.log(miles);
+
+// kilometers = miles * 1.60934;
+// let math = `${miles} miles is ${kilometers} kilometers`;
+
+// console.log(kilometers);
+// console.log(math);
+
+// console.log(typeof math);
+
+// let firstNum = prompt("first number");
+// let secondNum = prompt("second number");
+
+// b = firstNum;
+// c = secondNum;
+
+// if (b < c) {
+//     console.log("b is greater than c")
+// }
+// else {
+//     console.log("b is not greater than c")
+// }
+
+// var x = 19;
+
+// if (x) {
+//     console.log("number exists")
+//     }
+// if (x > 50) {
+//     console.log("number is larger than 50")
+// }
+// console.log(x)
+
+// let x = prompt("please give me a magic number");
+
+// let greaterThan10 = "number is greater than 10"
+// let equalTo10 = `${x} is equal to 10`;
+// let lessThan10 = "number is less than 10"
+
+// if (x > 10) {
+//     console.log(greaterThan10);
+//     alert(greaterThan10);
+// }
+// else if (x == 10) {
+//     console.log(equalTo10);
+//     alert(equalTo10);
+// }
+// else {
+//     console.log(lessThan10);
+//     alert(lessThan10);
+// }
+
+// x > 10 ? alert(greaterThan10) : alert(lessThan10);
+
+// age = 21
+
+// var club = (age >= 18) ? 'adult' : 'teen';
+
+// console.log(club);
+
+// var x = prompt("give me a number");
+
+// var even = (x % 2) ? 'odd' : 'even';
+
+// console.log(even);
+// console.log('remainder is ' + x % 2);
+
+// var x = prompt("give me x number");
+// var y = prompt("give me y number");
+
+// if (x > 10 && y > 10) {
+//     alert("this is true")
+// }
+// else {
+//     alert("this is not true")
+// }
+
+// var club = (x > 10 || y > 10) ? 'true' : 'false';
+
+// console.log(club)
+
+// let userAge = prompt("How old are you?");
+// let userMember = prompt("Are you a member?");
+
+// var ageTest = (userAge > 18 && userMember == true) ? 'Welcome' : 'Not welcome';
+
+// console.log(ageTest);
+
+// var time = prompt("What time is it?");
+// output = "";
+
+// if (time < 24 && time >= 18) {
+//     output = "good evening";
+// }
+// else if (time <= 17 && time >= 15)
+// {
+//     output = "good afternoon";
+// }
+// else{
+//     output = "good morning";
+// }
+
+// console.log(output);
+// alert(output);
+
+// let person = "Casey";
+
+// switch (person) {
+//     case "Bob":
+//         console.log(person +  " is not my friend");
+//         break;
+//     case "Casey":
+//         console.log(person + "I know this person") 
+// }
+
+// let expr = "Oranges";
+// let output = "";
+
+// switch (expr) {
+//     case 'Oranges':
+//       output = expr + ' are $0.59 a pound.';
+//       break;
+//     case 'Apples':
+//       console.log('Apples are $0.32 a pound.');
+//       break;
+//     case 'Bananas':
+//       console.log('Bananas are $0.48 a pound.');
+//       break;
+//     case 'Cherries':
+//       console.log('Cherries are $3.00 a pound.');
+//       break;
+//     case 'Mangoes':
+//     case 'Papayas':
+//       console.log('Mangoes and papayas are $2.79 a pound.');
+//       break;
+//     default:
+//       console.log('Sorry, we are out of ' + expr + '.');
+//   }
+//   console.log(`${output}
+// Is there anything else you'd like?`);
+
+
+
+// let dogs = "lab";
+// output = "";
+
+// switch (dogs) {
+//     case "maltese":
+//         output = "Maltese are white";
+//         console.log(output);
+//         alert(output);
+//         // break;
+//     case "lab":
+//         output = "labs are big dogs";
+//         break;
+//     case "bulldog":
+//         output = "bulldogs are funny";
+//         // break;
+//     // default:
+//     //     output = "sorry no dog was chosen";
+// }
+// console.log(output);
+
+// let water = "costco";
+
+// switch (water) {
+//     case "evian":
+//         console.log("evian was not my first choice");
+//         break;
+//     case "fuji":
+//         console.log("fugi is owned by coke");
+//         break;
+//     case "costco":
+//         console.log("costco is amazing");
+//         break
+// }
+// console.log(water + " is a great choice");
+
+// let output = "hello world";
+// let counter = 1;
+// welcome(output);
+// welcome(output);
+// welcome(output);
+
+// function welcome(message){
+//     counter++;
+//     let temp = counter + " times run";
+//     console.log(message);
+//     console.log(temp);
+// }
+
+// let myNum = 50;
+// addTen(myNum);
+
+// function addTen(num) {
+//     let temp = num;
+//     console.log(temp + 10);
+// }
+
+
+// let output = "hello world";
+// let counter = 0;
+// welcome(output);
+// welcome(output);
+// welcome(output);
+
+// function welcome(message) {
+//     counter++;
+//     let temp = counter + " times run";
+//     console.log(message);
+//     console.log(temp);
+// }
+
+// let realNum = 50;
+// addTen(realNum);
+
+// function addTen(num){
+//     let temp = num + 10;
+//     console.log(num);
+// }
+
+
+// create variables welcome, counter
+// 
+// run function 3 times increasing a variable 
+//
+//create function to output message
+// 
+// create variables for add ten
+// create function to add ten
+
+// var output = "hello world"; // message to output
+// var counter = 0; // counter to increase
+// welcome(output); //calling welcome function passing output parameter
+// welcome(output);
+// welcome(output);
+
+// function welcome(message){ //function taking message parameter
+//     counter++; //incremeting counter by 1
+//     temp = "has been run " + counter + " times"; //outputing to console counter being incremented
+//     console.log(message); 
+//     console.log(temp);
+// }
+
+// var realNum = 100; // creating realNum var and setting to 50
+// addTen(realNum); //running addTen function
+
+// function addTen(real){ //create addten function taking realnumber parameter
+//     temp = realNum + 180; // adding 10 to realnumber and assinging to temp var
+//     console.log(temp); // logging temp to console
+// }
+
+// adder1(30, 60);
+// adder1();
+// adder2(100, 100);
+// adder2(100);
+// adder3(50, 80);
+// adder3(100);
+
+
+// function adder1(numOne = 100, numTwo = 50) {
+//     console.log("number 1 " + numOne);
+//     console.log("number 2 " + numTwo);
+// }
+
+// function adder2(numOne, numTwo) {
+//     numTwo = numTwo || 5;
+//     console.log("number 1 " + numOne);
+//     console.log("number 2 " + numTwo);
+// }
+
+// function adder3(numOne, numTwo) {
+//     numTwo = typeof numTwo !== 'undefined' ? numTwo : 5;
+//     console.log("number 1 " + numOne);
+//     console.log("number 2 " + numTwo);
+// }
+
+// number1(50, 100);
+// // number1(50, );
+// number1();
+
+// function number1(num1 = 1, num2 = 10){
+//     // num2 = num2 || 5;
+//     console.log(num1);
+//     console.log(num2);
+// }
+
+
+
+
+
+
+// wheel1(235, 245);
+// wheel1(195, );
+
+// function wheel1(val1, val2){
+//     val2 = val2 || 275;
+//     console.log(val1);
+//     console.log(val2);
+// }
+
+// number1(10, 5);
+// number1();
+// number2(10, 5);
+// number2();
+// // number3(10, 5);
+// // number3();
+// // number4(10, 5);
+// // number4();
+
+// function number1(num1 = 10, num2 = 50){
+//     console.log("number 1 " + num1);
+//     console.log("number 2 " + num2);
+// }
+
+// function number2(num1, num2){
+//     num2 = num2 || 5;
+//     console.log("number 1 " + num1);
+//     console.log("number 2 " + num2);
+// }
+
+
+
+// let var1 = prompt("Give me number 1");
+// let var2 = prompt("Give me number 2");
+
+// function math(a, b){
+//     let multiply = a * b;
+//     alert("your total is " + multiply);
+//     // return multiply;
+// }
+
+// function message(){
+//     alert(`${var1} ${var2}`);
+// }
+
+// function counter(){
+//     number = 0;
+//     temp = number++;
+//     console.log(temp);
+// }
+
+// const btns = document.querySelectorAll('button');
+// console.log(btns);
+
+// let var1, var2, var3, var4;
+// var1 = var2 = var3 = var4 = 0;
+
+// btns[0].onclick = message1;
+// btns[1].onclick = message2;
+// btns[2].onclick = message3;
+// btns[3].onclick = message4;
+
+
+// function message1(){
+//     var1++;
+//     message();
+// }
+// function message2(){
+//     var2++;
+//     message();
+// }
+// function message3(){
+//     var3++;
+//     message();
+// }
+// function message4(){
+//     var4++;
+//     message();
+// }
+// function message(){
+//     console.log(var1 + " " + var2 + " " + var3 + " " + var4);
+// }
+
+// function math1(x){
+//     temp = x + 5;
+//     return temp;
+//   }
+// console.log(math1(90));
+
+// var math2 = function(x){
+//     temp = x + 5;
+//     return temp;
+// }
+// console.log(math2(91));
+
+// const btn = document.querySelectorAll('button');
+// btn[0].onclick = message;
+// // btn[3].onclick = message;
+
+// // function message(){
+// //     console.log("console log message function to let me know its run");
+// // }
+// // message();
+
+// let a = 5;
+// function message(){
+//     let a = 8;
+//     console.log(a);
+//     // let a = 10;
+// }
+
+
+
+// function loop(x){
+//     if (x >= 10) return;
+//     console.log(x);
+//     loop (x + 1);
+// }
+
+// loop(0);
+
+// var secretNumber = 5;
+// var guess;
+
+// loop();
+// function loop() {
+//     let guess = prompt("give me a number");
+//     if (guess == secretNumber) return;
+//     console.log("keep going");
+//     loop (guess + 1);
+// }
+
+// (function (firstName, lastName) {
+//     console.log("my name is " + firstName + lastName);
+// }) ("Casey ", "Foster");
+
+// let test = (function(){
+//     return "hello world";
+// });
+
+// console.log(test());
+
+
+// const test11 = (x) => x * 5;
+// console.log(test11(10));
+
+
+// const car = {
+//     year : 2019,
+//     make : "VW",
+//     model : "GTI",
+//     price : 20000
+// }
+
+// console.log("I just bought a " + car.year, car.make, car.model + " for $" + car.price);
+
+
+// const denae = {
+//     hair : "blonde",
+//     smile : "good",
+//     height : 55,
+//     age : 30,
+// }
+
+// const test = {
+//     b1 = denae.height + 1;
+// }
+
+// console.log(b1);
+// console.log(denae.height);
+
+// let car = prompt("give me some values");
+// console.log(car);
+
+// let raceCar = {};
+// car = raceCar.color;
+// console.log(car);
+// raceCar.weight = 1600;
+// raceCar.cool = "yes";
+// raceCar.cost = 20000;
+// console.log(raceCar.color);
+
+// var firstName = prompt("give me your first name");
+
+// firstName = [];
+
+// console.log(firstName);
+
+//  const myCar = {
+//      color:"orange",
+//      year:1997,
+//      model:"miata",
+//      price:20000,
+//      slow:"yes",
+//      turnOn:function(){console.log("started")},
+//      drive() {console.log("you are driving a " + this.year + " " + this.model)},
+//      brakes:function(){console.log("this is the brakes")}
+//  }
+
+// myCar.turnOn();
+// myCar.drive();
+// myCar.brakes();
+
+var numberLog = [];
+var mileLog = [];
+var addNumber = [];
+var num = [];
+var rangeCar = [];
+
+const btns = document.querySelectorAll('button');
+
+
+btns[0].onclick = converter;
+btns[1].onclick = mileMath;
+btns[2].onclick = carRange;
+btns[3].onclick = getNum;
+
+function converter() {
+    miles = prompt("how many miles?");
+    var math = miles * 1.6042;
+    mileLog.push(math);
+    document.getElementById('mileLog').innerHTML += mileLog[mileLog.length-1] + "<br />";
+}
+
+function mileMath() {
+    numberPrompt = prompt("Give me a number");
+    addNumber.push(numberPrompt);
+    document.getElementById('addNumber').innerHTML += addNumber[addNumber.length-1] + "<br />";
+}
+
+function carRange() {
+    mpg = prompt("how many miles per gallon do you get?");
+    gallons = prompt("how many gallons does your car hold?");
+    var math = mpg * gallons;
+    rangeCar.push(math);
+    document.getElementById('rangeCar').innerHTML += rangeCar[rangeCar.length-1] + "<br />";
+}
+
+function getNum(){
+    var num = prompt("info1");
+    numberLog.push(num);
+    document.getElementById('numberLog' + "miles").innerHTML += numberLog[numberLog.length-1] + "<br />";
+    console.log(numberLog);
+}
+
+const app = document.getElementById('root')
+
+const logo = document.createElement('img')
+logo.src = 'logo.png'
+
+const container = document.createElement('div')
+container.setAttribute('class', 'container')
+
+app.appendChild(logo)
+app.appendChild(container)
+
+var request = new XMLHttpRequest()
+request.open('GET', 'api.openweathermap.org/data/2.5/weather?zip=30339,us&appid=b6907d289e10d714a6e88b30761fae22', true)
+request.onload = function() {
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response)
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach(movie => {
+      const card = document.createElement('div')
+      card.setAttribute('class', 'card')
+
+      const h1 = document.createElement('h1')
+      h1.textContent = movie.title
+
+      const p = document.createElement('p')
+      movie.description = movie.description.substring(0, 300)
+      p.textContent = `${movie.description}...`
+
+      container.appendChild(card)
+      card.appendChild(h1)
+      card.appendChild(p)
+    })
+  } else {
+    const errorMessage = document.createElement('marquee')
+    errorMessage.textContent = `Gah, it's not working!`
+    app.appendChild(errorMessage)
+  }
+}
+
+request.send()
+
+
+
